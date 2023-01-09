22abf0737499cf062cfe221cd296716e7ff693ae
Michael Cee <112960233+michaelcee12@users.noreply.github.com>
2023-01-09 12:25:55 +0100

    Update Token.sol
    Token TimeLock

--- Token.sol
+++ Token.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.1;
 
 contract Token {
   string public name = "MC Token";
-  string public symbol = "MC";
+  string public symbol = "MCtk";
   uint256 public decimals = 18;
   uint256 public totalSupply;
   mapping(address => uint256) public balanceOf;
